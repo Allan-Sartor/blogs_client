c21cefe2198f6967e6cbc60b56efd6705f68e996
Allan <allan.sartor@pontomais.com.br>
2022-04-19 17:27:20 -0300

    New layout

--- src/components/Articles/List/ArticleList.js
+++ src/components/Articles/List/ArticleList.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { ArticleBox, Body, Container, Footer, Score } from './styles';
+import { ActionBox, ArticleBox, ArticleInfo, ButtonBox, Container, Footer, Score } from './styles';
 
 import { Rating } from '../../Rating/Rating';
+
 import { api } from '../../../services/api';
 
+import { FaEye, FaRegEdit } from 'react-icons/fa'
+
 export function ArticleList() {
-  const [ articles, setArticles ] = useState([])
+  const [articles, setArticles] = useState([])
 
   // Get data for articles
   useEffect(() => {
     async function getListArticles() {
       await api.get('articles')
         .then((response) => setArticles(response.data.data))
-        .catch((err) => 
+        .catch((err) =>
           alert('Não foi possivel listar todos os artigos! Servidor OFF', err)
         )
     }
@@ -23,26 +26,43 @@ export function ArticleList() {
 
   return (
     <Container>
-      { articles.map((i) => (
-          <ArticleBox key={i.id}>
-            <div>
-              <h1>{i.attributes.title}</h1>
-              <Body>
-                {i.attributes.body}
-              </Body>
-            </div>
+      {articles.map((i) => (
+        <ArticleBox key={i.id}>
+
+          <ArticleInfo>
+            <h1>{i.attributes.title}</h1>
+            <p>
+              {i.attributes.body}
+            </p>
+          </ArticleInfo>
+
+          <ActionBox>
+            <ButtonBox>
+              
+              <Link to={`/articles/${i.attributes.slug}`}>
+                <FaEye size={24} color="rgba(0, 0, 0, 8.6)" />
+              </Link>
+              <Link to={`/edit-article/${i.attributes.slug}`}>
+                <FaRegEdit size={24} color="rgba(0, 0, 0, 8.6)"/>
+              </Link>
+            </ButtonBox>
+
+            <Footer>
+              <span>
+                <p>Criado em:</p>
+                {
+                  new Intl.DateTimeFormat("pt-br")
+                    .format(new Date(i.attributes.created_at))
+                }
+              </span>
+            </Footer>
+
             <Score>
+              <p>Média de avalições:</p>
               <Rating score={i.attributes.avg_score.toFixed(1)} />
             </Score>
-            <Footer>
-              Data de criação:
-              {new Intl.DateTimeFormat("pt-br").format(
-                new Date(i.attributes.created_at)
-              )}
-            </Footer>
-            <Link to={`/articles/${i.attributes.slug}`}>Leia mais</Link>
-            <Link to={`/edit-article/${i.attributes.slug}`}>Editar artigo</Link>
-          </ArticleBox>
+          </ActionBox>
+        </ArticleBox>
       ))}
     </Container>
   )
--- src/components/Articles/List/styles.js
+++ src/components/Articles/List/styles.js
@@ -7,14 +7,46 @@ export const Container = styled.div`
 
 export const ArticleBox = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
 
   padding: 0 1rem 1rem;
   margin: 1rem 1rem 1rem 0;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   background-color: var(--shape);
- 
+  border: 0;
+  border-radius: 0.25rem;
+`;
+
+export const ActionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  width: 15rem;
+`
+
+export const Footer = styled.div`
+  p {
+    font-size: 1rem;
+    color: var(--text-body);
+  }
+`
+
+export const Score = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    color: var(--text-body);
+  }
+`
+
+export const ArticleInfo = styled.div`
+  width: 100%;
+  
   h1 {
     color: var(--text-title);
   }
@@ -22,34 +54,22 @@ export const ArticleBox = styled.div`
   p { 
     color: var(--text-body);
     height: 7.65rem;
-    overflow: hidden;
-    text-overflow: ellipsis;
   }
+`
+
+export const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 50%;
 
   a {
-    font-size: 0.75rem;
+    font-size: 1rem;
+
+    transition: filter 0.2s;
+    
+    &:hover {
+      filter: brightness(0.8);
+    }
   }
-
-  border: 0;
-  border-radius: 0.25rem;
-
-  /* transition: border 0.2s; */
-
-  /* &:hover {
-    border: 0.2rem solid var(--blue);
-    cursor: pointer;
-  }   */
-
-`;
-
-export const Score = styled.div`
-  
-`
-
-export const Footer = styled.div`
-  
-`
-
-export const Body = styled.div`
-  
 `
